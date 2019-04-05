0aa750a1a248fb55a0c4e44454e2089f9eab8916
Heloise Taillet <heloise96@hotmail.com>
2019-04-05 18:08:16 -0400

    backend stuff

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -8,8 +8,8 @@ class ToDoItemsController < ApplicationController
     u = User.find(params[:user_id])
     d = Day.find(params[:day_id])
     t = ToDoItem.create(description: params[:description], checked: params[:checked], priority: params[:priority],day: d, user: u)
-    Categorytodo.find_or_create_by(to_do_item: t,category_id: params[:category_id]))
-    render json: i
+    Categorytodo.find_or_create_by(to_do_item_id: t.id,category_id: params[:category_id])
+    render json: t
   end
 
 private
