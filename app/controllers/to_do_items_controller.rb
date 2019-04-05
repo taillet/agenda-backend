e9196ed19b3a09a225cab2d7d2b858af7da7026f
Heloise Taillet <heloise96@hotmail.com>
2019-04-05 17:24:09 -0400

    controller chhanges

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -3,4 +3,14 @@ class ToDoItemsController < ApplicationController
   @todoitems = ToDoItem.all
   render json: @todoitems
   end
+
+  def create
+    u = User.find(params[:user_id])
+    d = Day.find(params[:day_id])
+    t = ToDoItem.create(description: params[:description], checked: params[:checked], priority: params[:priority],day: d, user: u)
+    Categorytodo.find_or_create_by(to_do_item: t,category_id: params[:category_id]))
+    render json: i
+  end
+
+private
 end
