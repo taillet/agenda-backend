b2281bdd024f28cb65d6c3a53c79998d6a432324
Heloise Taillet <heloise96@hotmail.com>
2019-04-08 16:55:54 -0400

    added delete

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -12,5 +12,28 @@ class ToDoItemsController < ApplicationController
     render json: t
   end
 
-private
+  def update
+    t = ToDoItem.find(params[:id])
+    if params[:description] == nil
+      t.update(checked: params[:checked])
+    elsif params[:description] != nil
+      t.update(description: params[:description])
+    end
+    render json: t
+  end
+
+  def destroy
+  t = ToDoItem.find(params[:id])
+  t.destroy
+  render json: t
+end
+
+def delete_all
+  ToDoItem.all.each do |item|
+    if item.checked == true
+      item.destroy
+    end
+  end
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :categoryevents
   resources :days
   resources :users
+  post 'to_do_items/delete_all', to: "to_do_items#delete_all"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
