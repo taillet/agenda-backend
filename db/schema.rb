b1168c9180265a47e6e2b8d21880e7c6b5e1d695
Heloise Taillet <heloise96@hotmail.com>
2019-04-15 18:51:55 -0400

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2019_04_05_130435) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
